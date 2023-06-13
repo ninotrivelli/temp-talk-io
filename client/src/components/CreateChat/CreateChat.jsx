4834a09673e06662be3352bdaa73b66964717847
Nico <ninotrivelli@gmail.com>
2023-06-13 16:38:14 -0300

    andando

--- client/src/components/CreateChat/CreateChat.jsx
+++ client/src/components/CreateChat/CreateChat.jsx
@@ -1,19 +1,29 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./CreateChat.css";
 import { v4 as uuidv4 } from "uuid";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+// import ReCAPTCHA from "react-google-recaptcha";
 
 const CreateChat = () => {
   const [timer, setTimer] = useState(5);
   const navigate = useNavigate();
+  // const recaptchaRef = useRef();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (timer <= 0) return;
+
+    // const recaptchaValue = recaptchaRef.current.getValue();
+    // if (!recaptchaValue) {
+    //   toast.error("Verify you are human!");
+    //   return;
+    // }
+
     const chatId = uuidv4();
     navigate(`/chat/${chatId}`, {
-      state: { timer: timer * 60000, isHost: true },
+      state: { timer: timer * 60000, isHost: true }, // captcha: recaptchaValue ---> Mandar
     });
   };
 
@@ -33,6 +43,12 @@ const CreateChat = () => {
           max="120"
         />
       </div>
+
+      {/* <ReCAPTCHA
+        ref={recaptchaRef}
+        sitekey="your-site-key" // key for client side, provided by google. NEEDS DOMAIN FIRST.
+        onChange={(value) => console.log(value)}
+      /> */}
 
       <div className="cta__container">
         <button className="cta" type="submit">
